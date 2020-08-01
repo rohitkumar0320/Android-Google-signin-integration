6ba85c317c9df26689f0b644f07c4d3b7482b59d
Rohit kumar <rohitkumar0324rk@gmail.com>
2020-08-01 15:17:05 +0530

    Fist Google Auth

---
+++ app/src/main/java/com/example/signin/Main2Activity.java
@@ -0,0 +1,84 @@
+package com.example.signin;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class Main2Activity extends AppCompatActivity {
+    GoogleSignInClient mGoogleSignInClient;
+    Button sign_out;
+    TextView nameTV;
+    TextView emailTV;
+    TextView idTV;
+    ImageView photoIV;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        sign_out = findViewById(R.id.log_out);
+        nameTV = findViewById(R.id.name);
+        emailTV = findViewById(R.id.email);
+        idTV = findViewById(R.id.id);
+        photoIV = findViewById(R.id.photo);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(Main2Activity.this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            nameTV.setText("Name :" + personName);
+            emailTV.setText("Email: " + personEmail);
+            Glide.with(this).load(personPhoto).into(photoIV);
+            idTV.setText("session ID :" + personId);
+        }
+
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(Main2Activity.this, "Successfully signed out", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Main2Activity.this, MainActivity.class));
+                        finish();
+                    }
+                });
+    }
+}
